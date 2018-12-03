2b8b335a7a76732101d020060d177ac89f1b0521
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-03 11:03:12 -0500

    wp dynamic menus

--- themes/university-theme/functions.php
+++ themes/university-theme/functions.php
@@ -7,6 +7,12 @@
     }
     function university_features() {
         add_theme_support('title-tag'); //wp handles header title
+        addWPAdminMenuDisplayLocations();
+    }
+    function addWPAdminMenuDisplayLocations() {
+        register_nav_menu('headerMenuLocation', 'Header Menu Location');
+        register_nav_menu('footerLocationOne', 'Footer Location One'); 
+        register_nav_menu('footerLocationTwo', 'Footer Location Two');
     }
 
     add_action('wp_enqueue_scripts','load_scripts_and_styles');
