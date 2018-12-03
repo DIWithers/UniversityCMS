75f5fa036bde1b619fece9d977b9eb7cb62042a5
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-02 19:51:53 -0500

    dynamic side bar on parent and child pages

--- themes/university-theme/page.php
+++ themes/university-theme/page.php
@@ -26,13 +26,19 @@
                 </div>
             <?php  }
             ?>
-            <!-- <div class="page-links">
-                <h2 class="page-links__title"><a href="#">About Us</a></h2>
+            <div class="page-links">
+                <h2 class="page-links__title">
+                    <a href="<?php echo get_permalink($parentID) ?>"><?php echo get_the_title($parentID) ?></a>
+                </h2>
                 <ul class="min-list">
-                    <li class="current_page_item"><a href="#">Our History</a></li>
-                    <li><a href="#">Our Goals</a></li>
+                    <?php 
+                        wp_list_pages(array(
+                            'title_li' => NULL,
+                            'child_of' => $parentID ? $parentID : get_the_ID()
+                        ));
+                    ?>
                 </ul>
-            </div> -->
+            </div>
             <div class="generic-content">
                 <?php the_content(); ?>
             </div>
