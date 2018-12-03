b0f1f6d2a3eb52a36666a72a242b902cb48ac4f2
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-03 12:22:50 -0500

    Post information dynamic

--- themes/university-theme/index.php
+++ themes/university-theme/index.php
@@ -14,7 +14,13 @@
   <?php while(have_posts()) { the_post(); ?>
   <div class="post-item">
     <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-    <div class="metabox"><p> Posted by X on X in X </p></div>
+    <div class="metabox">
+      <p> 
+        Posted by <?php the_author_posts_link()?> 
+        on <?php the_time('n.j.y') ?> 
+        in <?php echo get_the_category_list(',')?> 
+      </p>
+    </div>
     <div class="generic-content">
       <?php the_excerpt(); ?>
       <p>
