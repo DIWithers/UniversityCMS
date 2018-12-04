13977e014a47fe684c97c9949f828157b7bb412d
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-03 20:46:17 -0500

    events now uses the WP archive feature to list all events

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -1,3 +1,4 @@
+<!-- Note: Hit Save in Settings -> Permalink if Page Not Found to update  -->
 <?php 
     function add_custom_post_types() {
         register_post_type('event', array(
@@ -9,7 +10,9 @@
                 'edit_item' => 'Edit Event',
                 'all_items' => 'All Events'
             ),
-            'menu_icon' => 'dashicons-calendar'
+            'rewrite' => array('slug' => 'events'),
+            'menu_icon' => 'dashicons-calendar',
+            'has_archive' => true
         ));
     }
     add_action('init', 'add_custom_post_types');
