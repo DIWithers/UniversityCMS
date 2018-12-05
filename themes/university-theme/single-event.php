eb3aece2c5be0d80ec5ca7592bcb66a58964122b
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-05 18:16:14 -0500

    correction to Events Home

--- themes/university-theme/single-event.php
+++ themes/university-theme/single-event.php
@@ -18,7 +18,7 @@
                 <p>
                     <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>">
                         <i class="fa fa-home" aria-hidden="true"></i> 
-                        Event Home
+                        Events Home
                     </a> 
                     <span class="metabox__main">
                         <?php the_title(); ?> 
