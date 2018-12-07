dc1de16dbcf55c86ad19ad66fe8e60323dc70b38
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-06 23:11:21 -0500

    use gulp watch to watch css and js upon changes

--- themes/university-theme/js/scripts.js
+++ themes/university-theme/js/scripts.js
@@ -8,4 +8,10 @@ import HeroSlider from './modules/HeroSlider';
 
 // Instantiate a new object using our modules/classes
 var mobileMenu = new MobileMenu();
-var heroSlider = new HeroSlider();
+var heroSlider = new HeroSlider();///Note///
+// Bundles into scripts-bundled.js
+// Upon changes, this file must be rebundled via command line:
+// theme-folder $ gulp watch
+// OR
+// theme-folder $ gulp scripts
+//The required files live in the app/public folder (gulpfile.js, package.json, settings.js, webpack.config.js) - https://github.com/LearnWebCode/vagrant-lamp
