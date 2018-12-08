38bd10cff36efbe6b4804e86b7196008a4ace64b
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-08 15:09:01 -0500

    watch search field for typing, only evaluate after typing stops for 2 sec

--- themes/university-theme/js/modules/Search.js
+++ themes/university-theme/js/modules/Search.js
@@ -5,14 +5,23 @@ class Search {
         this.openSearch = $(".js-search-trigger");
         this.closeSearch = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.searchField = $("#search-term");
         this.events();
         this.overlayIsOpen = false;
+        this.typingTimer;
     }
     events() {
         this.openSearch.on('click', this.openOverlay.bind(this));
         this.closeSearch.on('click', this.closeOverlay.bind(this));
         $(document).on('keydown', this.keyPressDispatcher.bind(this));
+        this.searchField.on('keydown', this.typingLogic.bind(this));
     }
+    typingLogic() {
+        clearTimeout(this.typingTimer);
+        this.typingTimer = setTimeout(() => {
+        }, 2000);
+    }
+
     keyPressDispatcher(event) {
         if (event.keyCode == 83 && !this.overlayIsOpen) { // S Key
             this.openOverlay();
