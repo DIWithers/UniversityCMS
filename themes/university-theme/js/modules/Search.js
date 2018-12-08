e5fa4e05934ab29716addef293a1fa3bd45d026d
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-08 14:47:33 -0500

    open and close search overlay with a key press

--- themes/university-theme/js/modules/Search.js
+++ themes/university-theme/js/modules/Search.js
@@ -6,10 +6,22 @@ class Search {
         this.closeSearch = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
         this.events();
+        this.overlayIsOpen = false;
     }
     events() {
         this.openSearch.on('click', this.openOverlay.bind(this));
         this.closeSearch.on('click', this.closeOverlay.bind(this));
+        $(document).on('keydown', this.keyPressDispatcher.bind(this));
+    }
+    keyPressDispatcher(event) {
+        if (event.keyCode == 83 && !this.overlayIsOpen) { // S Key
+            this.openOverlay();
+            this.overlayIsOpen = true;
+        }
+        if (event.keyCode == 27 && this.overlayIsOpen) { // Esc Key
+            this.closeOverlay();
+            this.overlayIsOpen = false;
+        }
     }
     openOverlay() {
         this.searchOverlay.addClass('search-overlay--active');
