1c1f7b85bd1a8ad01ce0cd0b477e19e248bcea35
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-12 09:14:52 -0500

    refactor; move js to proper file

--- themes/university-theme/js/modules/Search.js
+++ themes/university-theme/js/modules/Search.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 class Search {
     constructor() {
+        this.addSearchHtml();
         this.openSearch = $(".js-search-trigger");
         this.closeSearch = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
@@ -69,6 +70,22 @@ class Search {
     closeOverlay() {
         this.searchOverlay.removeClass('search-overlay--active');
         $("body").removeClass("body-no-scroll");
+    }
+    addSearchHtml() {
+        $("body").append(`
+            <div class="search-overlay">
+                <div class="search-overlay__top">
+                    <div class="container">
+                            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                            <input type="text" id="search-term" class="search-term" placeholder="What are you looking for?">
+                            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div class="container">
+                    <div id="search-over__results"></div>
+                </div>
+            </div>
+        `);
     }
 }
 export default Search;
