db5c6ede3bcc991ea2b9c3b86fd9a7fc15d1d0c2
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-27 12:59:15 -0500

    Remove Private: string from new notes; formatting for readability

--- themes/university-theme/page-my-notes.php
+++ themes/university-theme/page-my-notes.php
@@ -31,7 +31,11 @@
                     $userNotes->the_post();
                 ?>
                     <li data-id="<?php the_ID(); ?>">
-                        <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>" />
+                        <input 
+                            readonly 
+                            class="note-title-field" 
+                            value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>" 
+                        />
                         <span class="edit-note">
                             <i class="fa fa-pencil" aria-hidden="true"></i> Edit
                         </span>
