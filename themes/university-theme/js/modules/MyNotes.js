c93659c0236a4e449cca50ca5c1a8f0360bbbbcc
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-27 10:30:30 -0500

    init editing notes; toggle between readonly and editable

--- themes/university-theme/js/modules/MyNotes.js
+++ themes/university-theme/js/modules/MyNotes.js
@@ -7,6 +7,7 @@ class MyNotes {
 
     events() {
         $(".delete-note").on('click', this.deleteNote);
+        $(".edit-note").on('click', this.editNote.bind(this));
     }
     deleteNote(event) {
         var note = $(event.target).parents("li");
@@ -23,5 +24,22 @@ class MyNotes {
             }
         })
     }
+    editNote(event) {
+        var note = $(event.target).parents("li");
+        note.data('state') == 'editable' ? this.makeNoteReadOnly(note) : this.makeNoteEditable(note);
+    }
+    makeNoteEditable(note) {
+        note.find('.edit-note').html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
+        note.find('.note-title-field, .note-body-field').removeAttr('readonly').addClass("note-active-field");
+        note.find(".update-note").addClass("update-note--visible");
+        note.data('state', 'editable');
+    }
+    makeNoteReadOnly(note) {
+        note.find('.edit-note').html('<i class="fa fa-pencil" aria-hidden="true"></i> Edit');
+        note.find('.note-title-field, .note-body-field').attr('readonly', 'readonly').removeClass("note-active-field");
+        note.find(".update-note").removeClass("update-note--visible");
+        note.data('state', 'cancel');
+    }
+
 }
 export default MyNotes;
