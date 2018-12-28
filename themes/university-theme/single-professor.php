7392359f052327430a39461f3173d96703c73d88
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-28 12:48:35 -0500

    can now query and display Likes for Professors as well as if User Liked a Professor; Note that a Liked Professor ID field was created with ACF

--- themes/university-theme/single-professor.php
+++ themes/university-theme/single-professor.php
@@ -11,10 +11,37 @@
                 <div class="row group">
                     <div class="one-third"><?php the_post_thumbnail('professorPortrait'); ?></div>
                     <div class="two-thirds">
-                        <span class="like-box">
+                        <?php
+                            $likeCount = new WP_Query(array(
+                                'post_type' => 'like',
+                                'meta_query' => array(
+                                    array(
+                                        'key' => 'liked_professor_id',
+                                        'compare' => '=',
+                                        'value' => get_the_ID()
+                                    )
+                                )
+                            ));
+                            $likeExists = "no";
+                            $likeExistsQuery = new WP_Query(array(
+                                'author' => get_current_user_id(),
+                                'post_type' => 'like',
+                                'meta_query' => array(
+                                    array(
+                                        'key' => 'liked_professor_id',
+                                        'compare' => '=',
+                                        'value' => get_the_ID()
+                                    )
+                                )
+                            ));
+                            if ($likeExistsQuery->found_posts) {
+                                $likeExists = "yes";
+                            }
+                        ?>
+                        <span class="like-box" data-exists="<?php echo $likeExists; ?>">
                             <i class="fa fa-heart-o" aria-hidden="true"></i>
-                            <i class="fa fa-heart-" aria-hidden="true"></i>
-                            <span class="like-count">3</span>
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
                         </span>
                         <?php the_content(); ?>
                     </div>
