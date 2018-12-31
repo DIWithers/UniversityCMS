2d7705d01f927d3b7a2445c79f48c15ecb51c25b
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-12-31 00:45:43 -0500

    Limit to ONE like per professor; make sure ID belongs to a valid professor

--- themes/university-theme/includes/like-route.php
+++ themes/university-theme/includes/like-route.php
@@ -12,13 +12,29 @@ function likeRoutes() {
 function createLike($data) {
     if (is_user_logged_in()) {
         $professor = sanitize_text_field($data['professorId']);
-        return wp_insert_post(array(
+        $likeExistsQuery = new WP_Query(array(
+            'author' => get_current_user_id(),
             'post_type' => 'like',
-            'post_status' => 'publish',
-            'meta_input' => array(
-                'liked_professor_id' => $professor
+            'meta_query' => array(
+                array(
+                    'key' => 'liked_professor_id',
+                    'compare' => '=',
+                    'value' => $professor
+                )
             )
         ));
+        if ($likeExistsQuery->found_posts == 0 AND get_post_type($professor) == 'professor') {
+            return wp_insert_post(array(
+                'post_type' => 'like',
+                'post_status' => 'publish',
+                'meta_input' => array(
+                    'liked_professor_id' => $professor
+                )
+            ));
+        }
+        else {
+            die("Like already exists.");
+        }
     }
     else {
         die('You must be logged in to create a Like.');
